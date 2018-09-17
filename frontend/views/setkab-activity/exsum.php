1c5788d0bcaa8be2c3beecf60b7ba9a4c421dc0f
Ammar Fadhlur Rahman <afrastgeek@gmail.com>
2018-09-18 04:56:49 +0700

    Add kompetensigram in exsum page

--- frontend/views/setkab-activity/exsum.php
+++ frontend/views/setkab-activity/exsum.php
@@ -1,5 +1,6 @@
 <?php
 
+use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\redactor\widgets\Redactor as Redactor;
@@ -14,6 +15,90 @@ $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Setkab Activities'),
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 ?>
+
+<div class="setkab-activity-update">
+    <h1>Executive Summary</h1>
+</div>
+
+<div class="setkab-activity-update">
+    <div class="row">
+        <div class="col-md-6">
+            <h4>Diagram Kompetensi</h4>
+            <div class="radarChart" style="background-image: url('<?=Url::base()?>/images/setkab/LINGKARAN.png'); background-repeat: no-repeat;background-size: 490px 510px; height: 550px;"></div>
+        </div>
+    </div>
+
+    <script src="<?=Url::to('@web/js/radarChart.js');?>"></script>
+    <script src="<?=Url::to('@web/js/d3.min.js');?>"></script>
+
+    <script>
+
+      /* Radar chart design created by Nadieh Bremer - VisualCinnamon.com */
+
+            //////////////////////////////////////////////////////////////
+            //////////////////////// Set-Up //////////////////////////////
+            //////////////////////////////////////////////////////////////
+
+            var margin = {top: 53, right: 50, bottom: 35, left: 62},
+                width = Math.min(480, window.innerWidth - 10) - margin.left - margin.right,
+                height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
+
+            //////////////////////////////////////////////////////////////
+            ////////////////////////// Data //////////////////////////////
+            //////////////////////////////////////////////////////////////
+            var data = [
+            [
+                {axis:'',value:'3'},
+                {axis:'',value:'3'},
+                {axis:'',value:'3'},
+                {axis:'',value:'3'},
+                {axis:'',value:'3'},
+                {axis:'',value:'3'},
+                {axis:'',value:'3'},
+                {axis:'',value:'3'},
+                {axis:'',value:'3'},
+            ],[
+                {axis:'',value:'<?php echo $model->integritas_lki; ?>'},
+                {axis:'',value:'<?php echo $model->kerjasama_lki; ?>'},
+                {axis:'',value:'<?php echo $model->komunikasi_lki; ?>'},
+                {axis:'',value:'<?php echo $model->orientasihasil_lki; ?>'},
+                {axis:'',value:'<?php echo $model->pelayananpublik_lki; ?>'},
+                {axis:'',value:'<?php echo $model->pengembangandiri_lki; ?>'},
+                {axis:'',value:'<?php echo $model->pengelolaanperubahan_lki; ?>'},
+                {axis:'',value:'<?php echo $model->pengambilankeputusan_lki; ?>'},
+                {axis:'',value:'<?php echo $model->perekatbangsa_lki; ?>'},
+            ]
+            ];
+
+            //////////////////////////////////////////////////////////////
+            //////////////////// Draw the Chart //////////////////////////
+            //////////////////////////////////////////////////////////////
+
+            var color = d3.scale.ordinal()
+            .range(['#AEDFFB','#35274E']);
+
+            var radarChartOptions = {
+                w: width,
+                h: height,
+                margin: margin,
+                maxValue: 6,
+                levels: 5,
+                roundStrokes: false,
+                color: color,
+                opacityArea: 0.5,
+                opacityCircles: 0,
+                dotRadius: 3,
+                strokeWidth: 2,
+                wrapWidth: 10,
+                labelFactor: 10,
+            };
+            //Call function to draw the Radar chart
+            RadarChart(".radarChart", data, radarChartOptions);
+        </script>
+
+    <hr/>
+</div>
+
 <div class="setkab-activity-update">
 
 
