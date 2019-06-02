6eb5e7f8c76be7fe4c7ae80379d6cfd5937e4f94
root <root@jakarta.ppsdm.com>
2019-06-02 17:26:21 +0700

    include main-local and params-local in git

---
+++ common/config/main-local.php
@@ -0,0 +1,50 @@
+<?php
+return [
+    'components' => [
+  /* 
+         'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=ppsdm_paodb',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ], 
+    */                 
+ 
+           
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=ppsdm_paodb',
+            'username' => 'ppsdm',
+            'password' => 'ppsdM2014',
+            'charset' => 'utf8',
+        ], 
+
+		
+
+        'taodb' => [
+        
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=taodb',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+              'class' => 'Swift_SmtpTransport',
+              'host' => 'smtp.gmail.com',
+              'username' => 'gamantha.adhiguna@gmail.com',
+              'password' => 'gamantha123!',
+              'port' => '465',
+              'encryption' => 'ssl',
+          ],
+        ],
+    ],
+];
